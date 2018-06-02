442470311751252fcbc99b34b475a88b479c5a14
Brian LaRusso <brianlarusso@Brians-MacBook-Pro.local>
2018-06-01 20:56:59 -0400

    made sections for mac or linux install with -m or  -l flags

--- install.sh
+++ install.sh
@@ -2,16 +2,50 @@
 
 clear
 
-sudo apt-get install vim tmux powerline
-brew install vim tmux powerline
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #===================================================
 # mac specific configs
 #==================================================
+
+if ["$1" = "-m"]; then 
+  echo "mac mode activated"
+
+brew install vim tmux python
+pip install --user powerline-status
+
+brew install cmake
+else 
+  echo "not a mac"
+fi
+
+#====================================
+# Linux section 
+#====================================
+
+if ["$1" = "-l"]; then 
+
+  echo "Linux install"
+sudo apt-get install vim tmux powerline
+
+echo "installing xkeycaps"
+sudo apt-get install xkeycaps
+ln -s ~/dotfiles/.xmodmap-darkroom  ~/.xmodmap-`uname -n`  
+
+echo "setup xfce4 hot keys"
+
+ln -sf ~/dotfiles/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml 
+
+else 
+  echo "nothing to see here"
+fi
+
+#===================================================
+# general install section
+#==================================================
+
+# TPM tmux package manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #===================================================
 #alias
@@ -37,17 +71,10 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 echo "installing xkeycaps and linking .xmodmap-darkroom"
 
-sudo apt-get install xkeycaps
-ln -s ~/dotfiles/.xmodmap-darkroom  ~/.xmodmap-`uname -n`  
-
-echo "setup xfce4 hot keys"
-
-ln -sf ~/dotfiles/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml 
 
 #====================================
 # you complete me compiled component
 #====================================
-brew install cmake
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 #compile YCM
